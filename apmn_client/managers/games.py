4ebe2f906dce471f076f81003dc08d4fbd4979f8
Thanathip Limna <boatkrap@gmail.com>
2016-03-15 20:28:22 +0700

    fix for client

--- apmn_client/managers/games.py
+++ apmn_client/managers/games.py
@@ -4,27 +4,30 @@ from .base import Manager
 class GameManager(Manager):
     def __init__(self, client):
         super().__init__(client)
-
-    def update(self, method, args, qos=0):
         room_id = self.client.room.current_room['room_id']
-        topic = 'apaimanee/clients/{}/rooms/{}/update'.format(self.client.client_id, room_id)
+        self.topic = 'apaimanee/clients/{}/rooms/{}/update'.format(self.client.client_id, room_id)
+
+    def send_message(self, method, args, qos=0):
         request = dict(room_id=room_id, method=method, args=args)
 
-        self.client.publish(topic, request, qos)
+        self.client.publish(self.topic, request, qos)
 
     def ready(self):
         args = dict()
-        self.update('ready', args, 1)
+        self.send_message('ready', args, 1)
 
     def initial(self):
         args = dict()
-        self.update('initial', args, 1)
+        self.send_message('initial', args, 1)
+
+    def update(self, **kw):
+        args = kw
+        self.send_message('update', args, 0)
 
     def move_hero(self, x, y):
         args = dict(x=x, y=y)
-        self.update('move_hero', args)
+        self.send_message('move_hero', args)
 
     def skill_action(self, skill):
         args = dict(skill=skill)
-
-        self.update('skill_action', args)
+        self.send_message('skill_action', args)
